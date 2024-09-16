8a618a5ef9188266bf559cb6e9f0581b173c7237
qiubowen <3561288101@qq.com>
2024-09-16 16:31:23 +0800

    my nvim config for go

---
+++ lua/mappings.lua
@@ -0,0 +1,28 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map("n", "<leader>db", "<cmd> DapToggleBreakpoint <cr>", {desc = "Add breakpoint at line"})
+map("n", "<leader>dus", function ()
+  local widgets = require("dap.ui.widgets");
+  local sidebar = widgets.sidebar(widgets.scopes);
+  sidebar.open();
+end, { desc ="Open debugging sidebar"})
+map("n", "<leader><Down>", "<cmd> DapStepOver <cr>", {desc = "debug step over"})
+map("n", "<leader><Right>", "<cmd> DapStepInto <cr>", {desc = "debug step into"})
+map("n", "<leader><Left>", "<cmd> DapStepOut <cr>", {desc = "debug step out"})
+map("n", "<leader><f5>", "<cmd> DapContinue <cr>", {desc = "debug continue"})
+map("n", "<leader>gjt", "<cmd> GoTags json <cr>", {desc = "auto add json tag for go struct"})
+map("n", "<leader>gyt", "<cmd> GoTags yaml <cr>", {desc = "auto add yaml tag for go struct"})
+--map("n", "<leader>dgt", function ()
+--  require("dap-go").debug_test();
+--end, {desc = "Debug go test"})
+--map("n", "<leader>dgl", function ()
+--  require("dap-go").debug_last();
+--end, {desc="Debug last go test"})
+--
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function (_, opts)
+      require("dap-go").setup(opts)
+    end
+  },
+  {
+    "arp242/gopher.vim",
+    ft = "go",
+    config = function (_, opts)
+      --require("gopher").setup(opts)
+    end,
+    build = function ()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
